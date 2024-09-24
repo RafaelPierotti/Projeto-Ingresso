e0e3345b93a40f5dd117308ad2a86b0db549cc44
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-24 15:21:32 -0300

    Projeto final

--- js/app.js
+++ js/app.js
@@ -5,11 +5,36 @@ let inferior = 400;
 function buy (){
     let quantity = document.getElementById('quantity').value;
     let ticket = document.getElementById('ticket-type').value;
+    let ticketPista = document.getElementById('qtd-pista');
+    let ticketSuperior = document.getElementById('qtd-superior');
+    let ticketInferior = document.getElementById('qtd-inferior');
 
-    alert (quantity);
-    alert (ticket);
+    if (ticket == 'pista'){
+      
+        if (quantity > pista){
+            alert ('Valor inválido');
+        } else {
+            pista -= quantity;
+
+            ticketPista.textContent = `${pista}`;
+        }     
+   } else if (ticket == 'superior') {
+
+        if (quantity > superior){
+            alert ('Valor inválido');
+        } else {
+            superior -= quantity;
+    
+            ticketSuperior.textContent = `${superior}`;
+        }
+   } else if (ticket == 'inferior') { 
+
+        if (quantity > inferior){
+            alert ('Valor inválido');
+        } else {
+            inferior -= quantity;
+
+            ticketInferior.textContent = `${inferior}`;
+        }
+   }
 }
-
-let newPista = document.getElementById('qtd-pista');
-newPista.textContent = `${pista}`;
-
